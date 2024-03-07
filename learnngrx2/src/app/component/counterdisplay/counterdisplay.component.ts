06690ab56791ed5afdba3522820689bbb1428020
Kamila Adamska <76974781+kamadella@users.noreply.github.com>
2024-03-07 13:17:07 +0100

    display observable data using ASYNC pipe

--- learnngrx2/src/app/component/counterdisplay/counterdisplay.component.ts
+++ learnngrx2/src/app/component/counterdisplay/counterdisplay.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CounterMOdel } from '../../shared/store/counter.model';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-counterdisplay',
@@ -11,13 +12,16 @@ export class CounterdisplayComponent implements OnInit{
   constructor(private store:Store<{counter: CounterMOdel}>) {
 
   }
+
+
   counterDisplay!:number;
   channelName!:string;
+  countersubscribe!: Subscription;
+  counter$!:Observable<CounterMOdel>;
 
   ngOnInit(): void {
-    this.store.select('counter').subscribe(data=>{
-      this.counterDisplay = data.counter;
-      this.channelName = data.channelname
-    })
+    this.counter$ = this.store.select('counter');
   }
+  
+  
 }
